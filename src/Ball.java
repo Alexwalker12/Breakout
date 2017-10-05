86923c2da7358e877c074543386a14414d76543a
AlexWalker <alexjohn.walker@o2.com>
2017-10-05 15:54:24 +0100

    Progress on Class files

---
+++ src/Ball.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Ball extends Sprite implements Commons {
+
+    private int xdir;
+    private int ydir;
+
+    public Ball() {
+
+        xdir = 1;
+        ydir = -1;
+
+        ImageIcon ii = new ImageIcon("ball.png");
+        image = ii.getImage();
+
+        i_width = image.getWidth(null);
+        i_height = image.getHeight(null);
+
+        resetState();
+    }
+}
---
+++ src/Brick.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class Brick extends Sprite{
+
+    private boolean destroyed;
+
+    public Brick(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        ImageIcon ii = new ImageIcon("brick.png");
+        image = ii.getImage();
+
+        i_width = image.getWidth(null);
+        i_height = image.getHeight(null);
+
+        destroyed = false;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean val) {
+        destroyed = val;
+    }
+}
